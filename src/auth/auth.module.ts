156df822d1958b72178f7fab564c981961d8bea9
NhatBangLe <bangle37568@gmail.com>
2025-10-14 22:42:38 +0700

    change to use jose library

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,26 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
-import { JwtModule } from '@nestjs/jwt';
 import { PermissionsGuard } from './permission.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory(configService: ConfigService) {
-        return {
-          secret: configService.get('auth0.secret'),
-          verifyOptions: {
-            algorithms: ['RS256'],
-            audience: configService.get('auth0.audience'),
-            issuer: configService.get('auth0.issuer'),
-          },
-        };
-      },
-    }),
-  ],
+  imports: [ConfigModule],
   providers: [
     {
       provide: APP_GUARD,
