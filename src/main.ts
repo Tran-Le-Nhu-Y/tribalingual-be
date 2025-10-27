fd62846e43241e2cd4456120b007d61d145c3166
Tran-Le-Nhu-Y <tranlenhuy200503@gmail.com>
2025-10-27 22:32:51 +0700

    support SSL

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync(process.env.SSL_KEY_PEM as string),
+    cert: readFileSync(process.env.SSL_CERT_PEM as string),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   // CORS
   app.enableCors({
